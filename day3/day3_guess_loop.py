e5628926d4a7edcf30ee5602923d5ca9ea152733
juhan <wlswngks212@naver.com>
2025-05-14 23:52:55 +0900

    Day 3: 조건문 & 반복문 심화

---
+++ day3/day3_guess_loop.py
@@ -0,0 +1,8 @@
+secret = 7
+guess = 0
+while guess != secret:
+    guess = int(input("1부터 10 사이 숫자를 맞춰보세요: "))
+    if guess == secret:
+        print("정답!")
+    else:
+        print("틀렸습니다. 다시 시도하세요")
---
+++ day3/day3_sum.py
@@ -0,0 +1,4 @@
+total = 0
+for i in range(1, 101):
+    total += i
+    print("1부터 100까지의 합:", total)
