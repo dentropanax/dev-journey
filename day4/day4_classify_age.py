b6601b22344df08cf2d6a5c5b6834c90e2cf545a
juhan <wlswngks212@naver.com>
2025-05-24 16:48:23 +0900

    Day 4: 조건문 반복문 심화

---
+++ day4/day4_classify_age.py
@@ -0,0 +1,20 @@
+people = [
+    {"이름": "민수", "나이": 12},
+    {"이름": "영희", "나이": 17},
+    {"이름": "철수", "나이": 25},
+    {"이름": "지은", "나이": 8},
+    {"이름": "흥민", "나이": 31}
+]
+
+for person in people:
+    name = person["이름"]
+    age = person["나이"]
+
+    if age >= 30:
+        print(f"{name}은 중년입니다.")
+    if age >= 20:
+        print(f"{name}은 성인입니다.")
+    if age >= 14:
+        print(f"{name}은 청소년입니다.")
+    else:
+        print(f"{name}은 어린이입니다.")
